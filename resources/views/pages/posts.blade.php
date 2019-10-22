903297e92c56b9a12d3f414da05012ccbc4eb165
Joao Ferreira <jo4o.fds@gmail.com>
2019-10-22 03:53:59 -0300

    Update Posts View

--- resources/views/pages/posts.blade.php
+++ resources/views/pages/posts.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.default')
 @section('content')
 <div class="container">
-    @foreach ($posts as $post)
-        <div class="row">
-            <div class="card mt-5">
-                <div class="card-header">
-                  {{$title}}
-                </div>
-                <div class="card-body">
-                  <h5 class="card-title">{{$post->title}}</h5>
-                  <p class="card-text">{{$post->content}}</p>
-                  <a href="#" class="btn btn-primary">leia mais...</a>
-                  <hr>
-                </div>
-              </div>
+    <div class="row">
+        <div class="card mt-5">
+            <div class="card-header">
+                Posts
+            </div>
+                @foreach ($posts as $post)
+                    <div class="card-body">
+                        <h5 class="card-title">{{$post->title}}</h5>
+                        <p class="card-text">{{$post->content}}</p>
+                        <a href="#" class="btn btn-primary">leia mais...</a>
+                        <hr>
+                    </div>
+                @endforeach
         </div>
-    @endforeach
+    </div>
 </div>
 @stop
